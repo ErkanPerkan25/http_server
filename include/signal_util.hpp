a43e17bb5277787a4e72ef05e25339fffb50dc65
Eric <erichansson.united@gmail.com>
2025-08-16 16:22:32 -0400

    I am having issue with how many characters that are being read when the server reads the clients request

---
+++ include/signal_util.hpp
@@ -0,0 +1,4 @@
+#ifndef _SIGNAL_UTIL_HPP_
+#define _SIGNAL_UTIL_HPP_
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <csignal>
+#include <cstdio>
 #include <iostream>
 #include <netinet/in.h>
 #include <sys/socket.h>
@@ -5,6 +7,7 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <sstream>
+#include <fstream>
 
 using namespace std;
 
@@ -15,7 +18,7 @@ using namespace std;
 #define LISTEN_ERROR 4
 
 #define MAX_WAITING 25
-#define MAX_BUFFER_SIZE 4096
+#define MAX_BUFFER_SIZE 255
 
 
 int connectToServer(int port);
@@ -69,13 +72,14 @@ int connectToServer(int port){
 
 void processRequest(int conn_sock, struct sockaddr_in* client_addr){
     stringstream req; 
+    string method, path, version;
 
-    char buffer[MAX_BUFFER_SIZE];
-    unsigned int charsRead;
-    unsigned int totalCharsRead;
+    char buffer[81];
+    int charsRead;
+    int totalCharsRead;
 
     while(true){
-        charsRead = read(conn_sock, buffer, MAX_BUFFER_SIZE);
+        charsRead = read(conn_sock, buffer, 80);
         
         // Either failed or had nothing more to read
         if(charsRead <= 0)
@@ -85,18 +89,87 @@ void processRequest(int conn_sock, struct sockaddr_in* client_addr){
 
         totalCharsRead += charsRead;
 
-        if(charsRead < MAX_BUFFER_SIZE)
+        if(charsRead < 80)
             break;
     }
 
+    cout << req.str().length() << endl;
+
+    cout << "Total chars read: " << totalCharsRead << endl;
+
+    
     req.str()[totalCharsRead] = '\0';
 
     if(req.str()[totalCharsRead-1] == '\n')
         req.str()[totalCharsRead-2] = '\0';
+    
+    totalCharsRead = 0;
+
+    req >> method >> path;
+
+    if(method == "GET"){
+        cout << "Recived a 'GET' request! " << endl;
+        // Removes the leading . and / of the path
+        for(int i=0; i < path.size(); i++){
+            if(path[i] == '.' || path[i] == '/'){
+                path.erase(path.begin()+i);
+                i--;
+            }
+            else{
+                break;
+            }
+        }
+
+        path = "../html/"+path;
 
 
-    cout << req.str() << endl;
+        fstream fs;
+        fs.open(path);
+        if(fs.is_open()){
+            string buffer;
 
+            buffer += "HTTP/1.1 202 OK\n";
+            buffer += "Content-Type: text/html\n\r\n";
+
+            char ch;
+            while (fs.get(ch)) {
+                buffer += ch;
+            }
+
+            char* cbuff = (char *) buffer.c_str();
+            int needed = buffer.size();
+
+            while (needed) {
+                int n = write(conn_sock, cbuff, needed);
+                needed -= n;
+                cbuff += n;
+            }
+
+            fs.close();
+        }
+        else{
+            string buffer;
+
+            buffer += "404 NOT FOUND \r\n\r\n";
+            buffer += "<b>404 - Error resource not found on the server</b>";
+
+            char* cbuff = (char *) buffer.c_str();
+
+            int needed = buffer.length();
+
+            while(needed){
+                int n = write(conn_sock, cbuff, needed);
+                needed -= n;
+                cbuff += n;
+            }
+
+        }
+    }
+    else{
+        cerr << "405 Method not allowed! Stopping the request!\n";
+    }
+
+    cout << "Connection from " << inet_ntoa(client_addr->sin_addr) << endl;
 
     close(conn_sock);
 }
