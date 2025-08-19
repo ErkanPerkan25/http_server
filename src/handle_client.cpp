88674e226caee98d31023740a0c654dc387b3110
Eric <erichansson.united@gmail.com>
2025-08-19 12:20:31 -0400

    New approach is to parse the different stages of the HTTP protocol by themselves and within them parse what is needed. I'm doing this with a C-programming approach with dynamically allocating the memory.

--- src/handle_client.cpp
+++ src/handle_client.cpp
@@ -1,13 +1,16 @@
+#include <cstddef>
+#include <cstdlib>
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <iostream>
+#include <cstring>
 
 #include "handle_client.hpp"
 
 #define MAX_BUFFER 512
+#define CRLF "\r\n"
 
 typedef struct{
     char *method;
@@ -20,12 +23,46 @@ typedef struct{
     char *version;
     char *status;
     char *reason;
-} status_line;
+} status_line_t;
+
+typedef struct{
+    status_line_t status_line;
+    char *content_type;
+    uint32_t cotent_length;
+} http_res_t;
+
+int parse_http_req(char * msg, http_req_t *req){
+    // parse each line by using CRLF
+    char* saveptr; // stores the rest of the msg here
+    char* line = strtok_r(msg, CRLF, &saveptr);
+
+    // parse the words in the line
+    char *first_token_ptr;
+    char *method_var = strtok_r(line, " ", &first_token_ptr);
+    size_t method_size_len = strlen(method_var);
+    // Dynamic allocation of memory, set it to zero first
+    req->method = (char *)calloc(method_size_len+1, sizeof(char));
+    if(req->method == NULL) return -1;
+    // set the 'req->method' to method_var
+    strcpy(req->method, method_var);
+
+    // parse the url (path)
+    char *second_token_ptr;
+    char *uri = strtok_r(first_token_ptr, " ", &second_token_ptr);
+    size_t uri_size_len = strlen(uri);
+    req->uri = (char *)calloc(uri_size_len+1, sizeof(char));
+    if(req->uri == NULL) return -1;
+    strcpy(req->uri, uri);
+
+    // parse the version
+    
+    return 0;
+}
 
 int handle_client(int conn_sock){
 
-    http_req_t client_req {};
-    status_line status_line {};
+    http_req_t* client_req;
+    http_res_t* client_res;
 
     char buffer[MAX_BUFFER];
     int charsRead = 0;
@@ -49,7 +86,8 @@ int handle_client(int conn_sock){
         buffer[totatlCharsRead-2] = '\0';
     }
 
-    std::cout << buffer << std::endl;
+    parse_http_req(buffer, client_req);
+    //std::cout << buffer << std::endl;
 
     return close(conn_sock);
 }
