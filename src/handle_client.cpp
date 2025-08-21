0aff40f9af5d00756d05ef79516761e88a1584dd
Eric <erichansson.united@gmail.com>
2025-08-20 20:10:59 -0400

    Fixed issue for the parsing of the request

--- src/handle_client.cpp
+++ src/handle_client.cpp
@@ -1,4 +1,5 @@
 #include <cstddef>
+#include <cstdio>
 #include <cstdlib>
 #include <netinet/in.h>
 #include <sys/socket.h>
@@ -44,25 +45,44 @@ int parse_http_req(char * msg, http_req_t *req){
     req->method = (char *)calloc(method_size_len+1, sizeof(char));
     if(req->method == NULL) return -1;
     // set the 'req->method' to method_var
-    strcpy(req->method, method_var);
+    strncpy(req->method, method_var, method_size_len+1);
 
     // parse the url (path)
     char *second_token_ptr;
     char *uri = strtok_r(first_token_ptr, " ", &second_token_ptr);
     size_t uri_size_len = strlen(uri);
-    req->uri = (char *)calloc(uri_size_len+1, sizeof(char));
-    if(req->uri == NULL) return -1;
+    req->uri = (char *)calloc(uri_size_len, sizeof(char));
+    if(req->uri == NULL){
+        return -1;
+    }
     strcpy(req->uri, uri);
 
-    // parse the version
+    while(line != NULL){
+    
+    }
+
+    // parse the host
+    /*
+    line = strtok_r(saveptr, CRLF, &saveptr);
+
+    char* host_var;
+    char* host_h = strtok_r(line, " ", &host_var);
+    size_t  host_size_len = strlen(host_h);
+    req->host = (char*)calloc(host_size_len+1, sizeof(char));
+    if(req->host == NULL) return -1; 
+    strcpy(req->host, host_h);
+
+    line = strtok_r(saveptr, CRLF, &saveptr);
+    */
+
     
     return 0;
 }
 
 int handle_client(int conn_sock){
 
-    http_req_t* client_req;
-    http_res_t* client_res;
+    http_req_t client_req {};
+    http_res_t client_res {};
 
     char buffer[MAX_BUFFER];
     int charsRead = 0;
@@ -86,7 +106,7 @@ int handle_client(int conn_sock){
         buffer[totatlCharsRead-2] = '\0';
     }
 
-    parse_http_req(buffer, client_req);
+    parse_http_req(buffer, &client_req);
     //std::cout << buffer << std::endl;
 
     return close(conn_sock);
